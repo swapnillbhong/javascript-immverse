89af5ae526bd8c694a65d988ed5ed9fd40575b9d
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-07 16:21:32 +0530

    objects

---
+++ instanceobject.js
@@ -0,0 +1,5 @@
+var emp= new Object();
+emp.id=101;
+emp.name='swapnil bhong';
+emp.salary=20000;
+console.log(emp.id+" "+emp.name+" "+emp.salary);
---
+++ objectiterater.js
@@ -0,0 +1,2 @@
+emp={id:101,name:'swapnilbhong',salary:20000}
+console.log(emp.id+" "+emp.name+" "+emp.salary);
